9057f98557ce92d63808e3e6c27a2f34f01dc15a
magda <magdalena.kubincova@protonmail.com>
2023-05-05 11:38:02 +0200

    feat: basic file structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../styles/index.scss';
+	import { page } from '$app/stores';
+	import { Header } from '$components';
+</script>
+
+<svelte:head>
+	<title>Magda {$page.data.title ? `| ${$page.data.title}` : ''}</title>
+</svelte:head>
+
+<div id="app">
+	<Header />
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	#app {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+
+		.content {
+			flex: 1;
+		}
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,32 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import sveltePreprocess from 'svelte-preprocess';
+
+const filePath = dirname(fileURLToPath(import.meta.url));
+const sassPath = `${filePath}/src/styles/`;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	// preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			prependData: `@import '${sassPath}_variables.scss';`
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			$assets: 'src/assets'
+		}
 	}
 };
 
