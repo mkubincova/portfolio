509ec9d7887450b903c2c556e8a648ec38f8c607
magda <magdalena.kubincova@protonmail.com>
2023-05-10 17:29:01 +0200

    feat: zoom animation

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -18,7 +18,10 @@
 			<ul class="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 				{#each projects as project}
 					<li class="flex gap-2 items-center sm:flex-col sm:items-start">
-						<img src={project.cover} alt="" width="400" height="300" class="w-1/2 sm:w-auto" />
+						<div class="zoom-container w-1/2 sm:w-auto" style="--bkg-url: url({project.cover})">
+							<img src={project.cover} alt="" width="400" height="300" />
+						</div>
+
 						<div class="w-1/2 sm:w-auto">
 							<h2>{project.name}</h2>
 							<p class="text-xs">{project.shortText || ''}</p>
@@ -29,3 +32,30 @@
 		</div>
 	{/if}
 </article>
+
+<style lang="scss">
+	.zoom-container {
+		position: relative;
+		overflow: hidden;
+
+		&:before {
+			content: '';
+			position: absolute;
+			inset: 0 0 0 0;
+			background: var(--bkg-url) no-repeat center;
+			background-size: 115%;
+			clip-path: circle(0% at 100% 100%);
+			transition: clip-path 0.5s ease-in-out, background-size 0.5s ease-in-out;
+		}
+		&:hover:before {
+			clip-path: circle(150% at 100% 100%);
+			background-size: 120%;
+		}
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+	}
+</style>
