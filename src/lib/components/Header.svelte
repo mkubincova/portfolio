40392a997e626a454cbf7b8e92a69f352d126d06
magda <magdalena.kubincova@protonmail.com>
2023-05-10 17:19:05 +0200

    feat: toggle header on scroll

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,17 +8,56 @@
 		},
 		{
 			name: 'Projects',
-			path: '/about'
+			path: '/projects'
 		},
 		{
 			name: 'About',
 			path: '/about'
 		}
 	];
+
+	let scrollY: number;
+	let prevScroll = 0;
+
+	let direction = 0;
+	let prevDirection = 0;
+
+	let headerHeight: number;
+	let hide = false;
+
+	function handleScroll() {
+		/* Find the direction of scroll: 0 - initial, 1 - down, 2 - up */
+		if (scrollY > prevScroll) {
+			direction = 1;
+		}
+		if (scrollY < prevScroll) {
+			direction = 2;
+		}
+
+		/* Hide/show header if direction changed */
+		if (direction !== prevDirection) {
+			if (direction === 1 && scrollY > headerHeight) {
+				hide = true;
+				prevDirection = direction;
+			}
+			if (direction === 2) {
+				hide = false;
+				prevDirection = direction;
+			}
+		}
+
+		prevScroll = scrollY; // update scroll position
+	}
 </script>
 
-<header class="p-3 md:p-5 bg-slate-200 fixed top-0 left-0 right-0">
-	<nav class="flex justify-between gap-4 flex-wrap container">
+<svelte:window on:scroll={handleScroll} bind:scrollY />
+
+<header
+	class="site-header bg-slate-200 fixed top-0 left-0 right-0 z-10 transition-transform duration-200"
+	bind:clientHeight={headerHeight}
+	style={hide ? `transform: translateY(-${headerHeight}px)` : ''}
+>
+	<nav class="flex justify-between gap-4 flex-wrap container p-3 md:p-5">
 		<ul class="flex gap-4">
 			{#each mainMenu as item}
 				<li><a href={item.path}>{item.name}</a></li>
