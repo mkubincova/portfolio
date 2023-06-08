28814cca71a45ac1ed551819eb6b724d7eb5b571
magda <magdalena.kubincova@protonmail.com>
2023-06-08 13:29:32 +0200

    feat: open graph protocol

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,17 @@
 </script>
 
 <svelte:head>
-	<title>Magda {$page.data.title ? `| ${$page.data.title}` : ''}</title>
+	<title>Magda</title>
+	<meta property="og:title" content="Magdaléna Kubincová" />
+	<meta
+		property="og:description"
+		content="Selected work by Magdaléna Kubincová: web designer and developer."
+	/>
+	<meta
+		property="og:image"
+		content="https://res.cloudinary.com/dqorjojrw/image/upload/v1686223834/Portfolio%20site/mkubincova-cover_ye7tln.png"
+	/>
+	<meta property="og:url" content="https://mkubincova.vercel.app/" />
 </svelte:head>
 
 <div id="app" class="min-h-screen flex flex-col bg-[var(--color-site-bg)]">
