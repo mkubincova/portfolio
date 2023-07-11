9986023905a886ea347641d1d4305f4aa4e81fc8
Magda <mkubincova@proton.me>
2023-07-11 13:32:50 +0200

    Feat: portfolio projects update

--- src/lib/pages/Projects.svelte
+++ src/lib/pages/Projects.svelte
@@ -5,10 +5,28 @@
 
 	const projects: { name: string; link: string; cover: string; desc: string }[] = [
 		{
+			name: 'Recipe app',
+			link: 'https://recipe-app-frontend-iota.vercel.app/',
+			cover: 'v1685525154/Portfolio%20site/my-recipes.png',
+			desc: 'Cooking inspiration site build with Strapi and React.'
+		},
+		{
 			name: 'Pivovar Moravia',
 			link: 'https://pivovar-moravia.cz/',
 			cover: 'v1685525159/Portfolio%20site/moravia_rzibbh.png',
 			desc: 'Promotional web for local brewery'
+		},
+		{
+			name: 'Weather app',
+			link: 'https://weather-app-mkubincova.vercel.app/',
+			cover: 'v1689074561/Portfolio%20site/weather-app.png',
+			desc: 'Weather forecast website build with Vue3, Mapbox API and OpenWeather API.'
+		},
+		{
+			name: 'Holzmann machines',
+			link: 'https://holzmann-machines.eu/',
+			cover: 'v1685525154/Portfolio%20site/holzmann_qe2fbs.png',
+			desc: 'E-shop for sawing equipment distributor'
 		},
 		{
 			name: 'Bez úrazu',
@@ -16,12 +34,7 @@
 			cover: 'v1685525151/Portfolio%20site/bezurazu.cz_nasb4e.png',
 			desc: 'Presentation for non-profit organization'
 		},
-		{
-			name: 'Holzmann machines',
-			link: 'https://holzmann-machines.eu/',
-			cover: 'v1685525154/Portfolio%20site/holzmann_qe2fbs.png',
-			desc: 'E-shop for sawing equipment distributor'
-		}
+		
 	];
 </script>
 
